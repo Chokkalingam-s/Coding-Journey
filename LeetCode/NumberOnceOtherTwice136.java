46034bc9aa994aa63428dea62eb46804507c7bc6
Chokkalingam.s <chok22017.it@rmkec.ac.in>
2024-09-25 23:30:45 +0530

    XOR CONCEPT - Number Appearing Once in Array while other TWICE

---
+++ LeetCode/NumberOnceOtherTwice136.java
@@ -0,0 +1,21 @@
+public class NumberOnceOtherTwice136 {
+
+    public int singleNumber(int[] nums) {
+
+        int ans=0;
+        for(int i: nums)
+        {
+            ans ^= i;
+        }
+        return ans;
+        
+    }
+
+    public static void main(String[] args) {
+        NumberOnceOtherTwice136 obj = new NumberOnceOtherTwice136();
+        int[] nums = {4,1,2,1,2};
+        System.out.println(obj.singleNumber(nums));
+    }
+
+    
+}
